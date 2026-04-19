8853593de5282f435cac80dadedc7fb20fef5ba9
Gleb <bessolitsingleb@yandex.ru>
2026-04-19 13:46:16 +1000

    sss

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import create_engine
 
 data = np.random.uniform(0, 100, (3, 30, 3))
 
@@ -16,18 +16,17 @@ for i, layer in enumerate(layers):
             value = data[i, j, k]
             rows.append([cell_id, layer, j+1, coord, value])
 
-engine = create_async_engine("postgresql+asyncpg://aleksejbessolicin:password@localhost:5432/terminal", echo = True)
+engine = create_engine("postgresql+psycopg://aleksejbessolicin:password@localhost:5432/terminal")
 
 df = pd.DataFrame(rows, columns=['cell_id', 'layer', 'row_num', 'coord_type', 'value'])
 df.to_sql('container_storage', engine, if_exists='replace', index=False)
 
 print("Данные успешно загружены в PostgreSQL!")
 
-async_session = async_sessionmaker(engine, expire_on_commit= False, class_= AsyncSession)
 
 class Base(DeclarativeBase):
     pass
 
-async def get_db():
-    async with async_session() as session:
-        yield session
+# async def get_db():
+#     async with async_session() as session:
+#         yield session
